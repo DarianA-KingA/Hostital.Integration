cf62d2bcb917965c9f33dd859e42edb4e6d06c9f
DarianA-KingA <20210580@itla.edu.do>
2024-10-15 01:04:16 -0400

    Agrega diseño al home index

--- Hospital.Core/obj/Debug/net8.0/Hospital.Core.AssemblyInfo.cs
+++ Hospital.Core/obj/Debug/net8.0/Hospital.Core.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3dae4024e64611c87a523b83447a5b1da19ad5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9de892924affae92778a562d282f8d508590a2a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
